3f908530e860acdcdd8afb8b8c19e96b877c06f3
yusuf963 <mohammad963yusuf@gmail.com>
2024-01-07 22:10:20 +0000

    Install tooling

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // require: ['ts-node/register', 'tsconfig-paths/register'],
-  recursive: true,
-  extension: ['ts'],
-  timeout: 15000,
-  exit: true
+	// require: ['ts-node/register', 'tsconfig-paths/register'],
+	recursive: true,
+	extension: ['ts'],
+	timeout: 15000,
+	exit: true,
 };
