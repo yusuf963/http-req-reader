ae43b8582d27009e5d388b97bb5e490b48bf599d
yusuf963 <mohammad963yusuf@gmail.com>
2024-03-01 21:24:55 +0000

    Change function arg to array of string instead of string

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,45 @@
 import { RequestInfo, RequestInfoParams } from './types';
 export function paresCustomRequestId({
 	req,
-	customRequestIdHeader,
+	customRequestHeader,
 }: RequestInfoParams) {
-	if (!customRequestIdHeader) return '';
+	if (!customRequestHeader || customRequestHeader.length === 0)
+		return 'No custom request header passed in';
 	const result = {};
-	result[customRequestIdHeader] = req.headers[customRequestIdHeader] || '';
+	for (const header of customRequestHeader) {
+		result[header] = req.headers[header];
+	}
 	return result;
 }
 
-export function requestInfo({ req, customRequestIdHeader }: RequestInfoParams): RequestInfo {
-	const ip = req.ip || req.connection.remoteAddress;
-	const userAgent = req.get('user-agent');
-	const browser = userAgent.split(' ')[0];
-	const timestamp = new Date().toLocaleString();
+export function requestInfo({
+	req,
+	customRequestHeader,
+}: RequestInfoParams): RequestInfo {
 	const url = req.url;
-	const queryParams = req.query;
+	const cookies = req.cookies;
 	const method = req.method;
-	const referrer = req.get('referrer') || req.get('referer');
 	const hostname = req.hostname;
 	const protocol = req.protocol;
-	const cookies = req.cookies;
+	const queryParams = req.query;
+	const userAgent = req.get('user-agent');
+	const browser = userAgent.split(' ')[0];
 	const headers = req.headers;
 	const languages = req.acceptsLanguages();
-	const customRequestId = paresCustomRequestId({ req, customRequestIdHeader });
+	const timestamp = new Date().toLocaleString();
+	const referrer = req.get('referrer') || req.get('referer');
+	const customRequestHeaders = paresCustomRequestId({
+		req,
+		customRequestHeader,
+	});
+	const ip = req.ip || req.connection.remoteAddress; // socket or connection, but connect is depricated
 	const os =
 		req.get('sec-ch-ua-platform') + ' ' + req.get('user-agent').split(' ')[1];
 	// const geolocation = geoip.lookup(ip);
 
 	return {
 		timestamp,
-		customRequestId,
+		customRequestHeaders,
 		languages,
 		protocol,
 		method,
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,24 +1,24 @@
 import { Request } from 'express';
 
 export type RequestInfoParams = {
-    req: Request;
-    customRequestIdHeader?: string;
+	req: Request;
+	customRequestHeader?: string[];
 };
 
 export type RequestInfo = {
-    timestamp: string;
-    customRequestId: object;
-    languages: string[];
-    protocol: string;
-    method: string;
-    hostname: string;
-    url: string;
-    queryParams: import('qs').ParsedQs;
-    headers: import('http').IncomingHttpHeaders;
-    cookies: object;
-    ip: string;
-    referrer: string;
-    browser: string;
-    userAgent: string;
-    os: string;
-}
+	timestamp: string;
+	customRequestHeaders: object;
+	languages: string[];
+	protocol: string;
+	method: string;
+	hostname: string;
+	url: string;
+	queryParams: import('qs').ParsedQs;
+	headers: import('http').IncomingHttpHeaders;
+	cookies: object;
+	ip: string;
+	referrer: string;
+	browser: string;
+	userAgent: string;
+	os: string;
+};
